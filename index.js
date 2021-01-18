1f2af659da0082ac2eff4c870ca0ca020a91bd5c
Pete-Scale <petescale@gmail.com>
2021-01-18 13:52:55 -0600

    note for questions, 1st question

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const inquirer = require('inquirer');
 
 // array of questions for user
 const questions = [
-
+    // Title, Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+    {
+        type: 'insert',
+        message: "What's the title of your project?",
+        name: 'title'
+    },
 ];
 
 // function to write README file
