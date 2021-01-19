06f05798ca131773245c3306d8aee27ed1c3e4c1
Pete-Scale <petescale@gmail.com>
2021-01-19 10:07:33 -0600

    title, description, table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,28 @@
 const generateMarkdown = (answers) => {
   const { title, description, installation, usage, license, credits, tests, github, email } = answers
   return `# ${title}
+          ![badge](https://img.shields.io/badge/license-${license}-brightgreen)
+          <br>
+          ## Description
+          ${description}
+          ---
+          ## Table of Contents
+
+          [Description](#description)
+
+          [Installation](#installation)
+
+          [Usage](#usage)
+
+          [License](#license)
+
+          [Credits](#credits)
+
+          [Tests](#tests)
+
+          [Questions](#questions)
+          ---
+
 
   `;
 }
